de4821be848dc8ad9fd15beb4d52342be25202f2
btupan <115275767+btupan@users.noreply.github.com>
2022-10-15 17:53:45 +0200

    Add files via upload

---
+++ Dobbelsteen.py
@@ -0,0 +1,25 @@
+import tkinter
+import random
+from tkinter import *
+
+root = tkinter.Tk()
+root.geometry('750x550')
+root.title('Roll Dice')
+
+Namen = ["Ad", "Boaz", "Remi", "William"]
+label = tkinter.Label(root, text='', font=('Helvetica', 260))
+
+label1 = tkinter.Label(root, text='', font=('Helvetica', 100))
+label1.place(x=70,y=50)
+
+def roll_dice():
+    dice = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
+    label.configure(text=f'{random.choice(dice)}')
+    label.pack()
+def genereer_namen():
+    label1.configure(text=f'{random.choice(Namen)}')
+    label1.pack()
+
+button = tkinter.Button(root, text='roll dice', foreground='blue', command=lambda:[roll_dice(), genereer_namen()])
+button.place(x=820,y=10)
+root.mainloop()
